dfb0dad661c505d3eb6414060c9897d2cad76d52
Hatice Kovan <83229676+haticekovan@users.noreply.github.com>
2024-02-02 17:05:18 +0300

    Update 8*8led.ino

--- 88led/88led.ino
+++ 88led/88led.ino
@@ -1,3 +1,5 @@
+//It lights the LEDs with various combinations.Please make sure that a row value is low and a col value is high. 
+
 const int row[8] = { 2, 3,4,5,6,7,8,9 };
 const int col[8] = { 12,13,14,15,16,17,18,19 };
 
@@ -15,17 +17,21 @@ void setup() {
 void loop() {
   for (int x = 0; x < 8; x++) {
     for (int y = 8; y >=0; y--) {
-     // digitalWrite(row[x], LOW);
-      digitalWrite(row[y], LOW);     // turn on the row
+
+    //one by one led burning
+      digitalWrite(row[x], LOW);
       digitalWrite(col[y], HIGH);
-      // digitalWrite(row[y+1], LOW); 
-      // digitalWrite(row[y+2], LOW);
-      // digitalWrite(row[5-y], LOW);
-      // digitalWrite(row[6-y], LOW); 
+
+      //cross led burning
+      // digitalWrite(row[y], LOW);     
+      //digitalWrite(col[y], HIGH);    
+
+      // cross led burning from the bottom side
       // digitalWrite(row[7-y], LOW);     // turn on the row
       // digitalWrite(col[7-y], HIGH);
-      //digitalWrite(col[y+1], HIGH);      // turn on the column (LED)
-      delay(2000);   
+      
+
+      delay(1000);   
       for (int i = 0; i < 8; i++) {
         digitalWrite(row[i], HIGH);   // turn off all rows
         digitalWrite(col[i], LOW);  // turn off all columns (LEDs)
